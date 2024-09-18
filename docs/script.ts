a3854d48d4df8436a54a6688e5b84e4be84915e9
Valhryn <yoav.maoz0@gmail.com>
2024-09-18 18:39:50 -0400

    change background to black instead

--- docs/script.ts
+++ docs/script.ts
@@ -2,4 +2,7 @@ const btn = document.querySelector("databaseLoaded");
 
 btn?.addEventListener("click", () => {
     btn.nodeValue = "Database Loaded";
+
+    const rndCol = `rgb(0, 0, 0)`;
+    document.body.style.backgroundColor = rndCol;
 })
